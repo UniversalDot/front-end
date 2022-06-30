d9951675306961894199d1605506132070ffa60d
Atanasov, Slave <slave.atanasov@ext.illimity.com>
2022-07-01 00:04:04 +0200

    major fix - dev and prod working fully

--- config-overrides.js
+++ config-overrides.js
@@ -15,23 +15,50 @@
 //             Buffer: ["buffer", "Buffer"],
 //         }),
 //     ]
-//     // console.log(config.resolve)
-//     // console.log(config.plugins)
-
 //     return config
 // }
 
+// const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
+// module.exports = function override(config, env) {
+//   config.resolve = {
+//     fallback: {
+//       stream: require.resolve('stream-browserify'),
+//     },
+//     // NOTE: I added this; missing in Substrate template and causes missing module errors;
+//     extensions: [...config.resolve.extensions, '.ts', '.js'],
+//   };
+
+//   config.plugins.push(new NodePolyfillPlugin());
+//   return config;
+// };
+
+// Leave commented out configs above for a certain period as an archive of what we tried earlier that didn't work in build mode;
+// node-polyfill-webpack-plugin - this depency is installed because it contains process, buffer, util and other sub-dependencies, so we don't add them as deps separately;
+
+// const webpack = require('webpack');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-module.exports = function override(config, env) {
-  config.resolve = {
-    fallback: {
-      stream: require.resolve('stream-browserify'),
-    },
-    // NOTE: I added this; missing in Substrate template and causes missing module errors;
-    extensions: [...config.resolve.extensions, '.ts', '.js'],
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    process: require.resolve('process/browser'),
+    zlib: require.resolve('browserify-zlib'),
+    stream: require.resolve('stream-browserify'),
+    util: require.resolve('util'),
+    buffer: require.resolve('buffer'),
+    asset: require.resolve('assert'),
   };
 
+  // Option 1 that works - use either one;
+  // config.plugins.push(
+  //   new webpack.ProvidePlugin({
+  //     process: 'process/browser.js',
+  //     Buffer: ['buffer', 'Buffer'],
+  //   })
+  // );
+
+  // Option 2 that works - use either one;
   config.plugins.push(new NodePolyfillPlugin());
+
   return config;
 };
