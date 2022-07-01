7740975a9517c4cb743300e80bbde21dbb677f6e
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2022-07-01 16:19:40 +0200

    fix for mac

--- config-overrides.js
+++ config-overrides.js
@@ -36,29 +36,31 @@
 // Leave commented out configs above for a certain period as an archive of what we tried earlier that didn't work in build mode;
 // node-polyfill-webpack-plugin - this depency is installed because it contains process, buffer, util and other sub-dependencies, so we don't add them as deps separately;
 
-// const webpack = require('webpack');
+const webpack = require('webpack');
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = function override(config) {
   config.resolve.fallback = {
+    ...config.resolve.fallback,
     process: require.resolve('process/browser'),
     zlib: require.resolve('browserify-zlib'),
     stream: require.resolve('stream-browserify'),
     util: require.resolve('util'),
     buffer: require.resolve('buffer'),
     asset: require.resolve('assert'),
+    console: require.resolve('console-browserify'),
   };
 
   // Option 1 that works - use either one;
-  // config.plugins.push(
-  //   new webpack.ProvidePlugin({
-  //     process: 'process/browser.js',
-  //     Buffer: ['buffer', 'Buffer'],
-  //   })
-  // );
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser.js',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  );
 
   // Option 2 that works - use either one;
-  config.plugins.push(new NodePolyfillPlugin());
+  // config.plugins.push(new NodePolyfillPlugin());
 
   return config;
 };
