e0c1bdcaa9317e10635da40242aed99b30a06760
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2022-10-14 10:30:01 +0200

    add project id

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "zo1vt2",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
